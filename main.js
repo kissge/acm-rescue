edde09c3c139438e82d7811aa90d31c8cfeed966
kissge <mail@yo.eki.do>
2015-10-09 17:07:51 +0900

    Fix the infinite loop problem

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-var url = 'http://dl.acm.org/citation.cfm?id=' + location.href.split('/')[5];
-var done = function (res) {
-    if (/FullTextPDF.*href="(.*?)"/i.test(res)) {
-        location.href = 'http://dl.acm.org/' + RegExp.$1;
-    } else {
-        error();
-    }
-};
-var error = function () {
-    $('body').append($('<a>Were you looking at this: ' + url + '</a>').prop('href', url));
-};
+if (document.title === 'Access Denied') {
+    var url = 'http://dl.acm.org/citation.cfm?id=' + location.href.split('/')[5];
+    var done = function (res) {
+        if (/FullTextPDF.*href="(.*?)"/i.test(res)) {
+            location.href = 'http://dl.acm.org/' + RegExp.$1;
+        } else {
+            error();
+        }
+    };
+    var error = function () {
+        $('body').append($('<a>Were you looking at this: ' + url + '</a>').prop('href', url));
+    };
 
-$.ajax({url: url})
-    .done(done)
-    .fail(error);
+    $.ajax({url: url})
+        .done(done)
+        .fail(error);
+}
